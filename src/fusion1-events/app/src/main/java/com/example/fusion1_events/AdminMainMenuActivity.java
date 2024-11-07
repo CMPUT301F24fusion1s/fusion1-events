27af96fb76bfb24ceb5d5a03a6fede57b6c5d8df
MoTheCracker31 <113314673+MoTheCracker31@users.noreply.github.com>
2024-11-07 12:34:23 -0700

    Documented Admin, AdminMainMenue, and DeviceManager classes

--- src/fusion1-events/app/src/main/java/com/example/fusion1_events/AdminMainMenuActivity.java
+++ src/fusion1-events/app/src/main/java/com/example/fusion1_events/AdminMainMenuActivity.java
@@ -5,14 +5,22 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 public class AdminMainMenuActivity extends AppCompatActivity {
 
+    /**
+     * Called when the activity is first created. This method sets up the layout and initializes the UI components
+     * for the main menu that the admin interacts with.
+     *
+     * @param savedInstancesState If the activity is being re-initialized after previously being shut down,
+     *                            this Bundle contains the most recent data supplied.
+     */
     @Override
     protected void onCreate(Bundle savedInstancesState){
        super.onCreate(savedInstancesState);
        setContentView(R.layout.activity_admin_main_menu);
 
-       // buttons
+        // Initialize buttons to interact with different sections of the admin main menu
         Button viewEventButton = findViewById(R.id.btn_view_event);
         Button viewProfilesButton = findViewById(R.id.btn_view_profiles);
         Button viewFacilitiesButton = findViewById(R.id.btn_view_facilities);
